4cccb061d190f416e99c3de401aa13b46d457a60
VictorRubinec <victor.rubinec@sptech.school>
2022-06-04 23:26:08 -0300

    ajustes css / função mostrar senha

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -2,14 +2,14 @@
 // sessão
 function validarSessao() {
     // aguardar();
-  
+
     var usuarioSession = sessionStorage.USUARIO;
     console.log(usuarioSession)
     var dadosUsuario = JSON.parse(usuarioSession)
     console.log(dadosUsuario.nome)
- 
-   
-    
+
+
+
     var h1LoginUsuario = document.getElementById("h1_login_usuario");
     /* nomeUsuario.value = nome;
      sobrenomeUsuario.value = sobrenome;
@@ -22,7 +22,7 @@ function validarSessao() {
         if (h1LoginUsuario != undefined) {
             h1LoginUsuario.innerHTML = dadosUsuario.email;
         }
-        b_usuario.innerHTML = 'Olá, '+dadosUsuario.nome;
+        b_usuario.innerHTML = 'Olá, ' + dadosUsuario.nome;
         acabarCarregar()
         // finalizarAguardar();
     } else {
@@ -31,14 +31,14 @@ function validarSessao() {
 }
 function validarSessao2() {
     // aguardar();
-  
+
     var usuarioSession = sessionStorage.USUARIO;
     console.log(usuarioSession)
     var dadosUsuario = JSON.parse(usuarioSession)
     console.log(dadosUsuario.nome)
- 
-   
-    
+
+
+
     var h1LoginUsuario = document.getElementById("h1_login_usuario");
     /* nomeUsuario.value = nome;
      sobrenomeUsuario.value = sobrenome;
@@ -51,7 +51,7 @@ function validarSessao2() {
         if (h1LoginUsuario != undefined) {
             h1LoginUsuario.innerHTML = dadosUsuario.email;
         }
-        b_usuario.innerHTML = 'Olá, '+dadosUsuario.nome;
+        b_usuario.innerHTML = 'Olá, ' + dadosUsuario.nome;
         //acabarCarregar()
         // finalizarAguardar();
     } else {
@@ -64,7 +64,7 @@ function validarSessao2() {
 function dadosUsuario() {
     var usuarioSession = sessionStorage.USUARIO;
     var dadosUsuario = JSON.parse(usuarioSession)
-    
+
     if (dadosUsuario.cargo != "Administrador") {
         empresaA.style.display = "none";
         funcionarioA.style.display = 'none';
@@ -72,25 +72,25 @@ function dadosUsuario() {
     }
     var h1LoginUsuario = document.getElementById("h1_login_usuario");
     nomeUsuario.value = dadosUsuario.nome;
-     sobrenomeUsuario.value = dadosUsuario.sobrenome;
-     cargoUsuario.value = dadosUsuario.cargo;
-     usuario.value = dadosUsuario.usuario;
-     senhaUsuario.value = dadosUsuario.senha;
+    sobrenomeUsuario.value = dadosUsuario.sobrenome;
+    cargoUsuario.value = dadosUsuario.cargo;
+    usuario.value = dadosUsuario.usuario;
+    senhaUsuario.value = dadosUsuario.senha;
     if (dadosUsuario.usuario != null && dadosUsuario.nome != null) {
         // window.alert(`Seja bem-vindo, ${nome}!`);
         if (h1LoginUsuario != undefined) {
             h1LoginUsuario.innerHTML = dadosUsuario.email;
         }
-        b_usuario.innerHTML = 'Olá, '+dadosUsuario.nome;
+        b_usuario.innerHTML = 'Olá, ' + dadosUsuario.nome;
 
         // finalizarAguardar();
     } else {
         console.log(dadosUsuario + " não está logado")
-       // window.location = "../login.html";
+        // window.location = "../login.html";
     }
-   acabarCarregar();
+    acabarCarregar();
 }
-function acabarCarregar(){
+function acabarCarregar() {
     carregamento.style.display = "none";
     cont.style.display = "flex";
 }
@@ -108,11 +108,11 @@ function aguardar() {
     divAguardar.style.display = "flex";
 }
 
-function dadosEmpresa(){
+function dadosEmpresa() {
 
     var usuarioSession = sessionStorage.USUARIO;
     var dadosUsuario = JSON.parse(usuarioSession)
-    var empresaSession = sessionStorage.EMPRESA 
+    var empresaSession = sessionStorage.EMPRESA
     var dadosEmpresa = JSON.parse(empresaSession)
     console.log(dadosEmpresa)
     var enderecoSession = sessionStorage.ENDERECO
@@ -124,16 +124,16 @@ function dadosEmpresa(){
         document.getElementById('cargoUsuario').setAttribute('disabled', 'disabled');
     }
     console.log(dadosUsuario.fkEmpresa)
-    
+
     if (dadosUsuario.usuario != null && dadosUsuario.nome != null) {
         // window.alert(`Seja bem-vindo, ${nome}!`);
-      
-        b_usuario.innerHTML = 'Olá, '+dadosUsuario.nome;
+
+        b_usuario.innerHTML = 'Olá, ' + dadosUsuario.nome;
 
         // finalizarAguardar();
     } else {
         console.log(dadosUsuario + " não está logado")
-       // window.location = "../login.html";
+        // window.location = "../login.html";
     }
 
     console.log(dadosEndereco)
@@ -142,7 +142,7 @@ function dadosEmpresa(){
     cnpjEmpresa.value = dadosEmpresa.cnpj;
     emailEmpresa.value = dadosEmpresa.email;
     telCel.value = dadosEmpresa.telefone_c;
-    telFix.value = dadosEmpresa.telefone_f;   
+    telFix.value = dadosEmpresa.telefone_f;
     cep.value = dadosEndereco.cep;
     logradouro.value = dadosEndereco.logradouro;
     num.value = dadosEndereco.numero;
@@ -153,8 +153,90 @@ function dadosEmpresa(){
     acabarCarregar()
 }
 
-function obterDadosUltimaHora(idSensor){
-    
+function obterDadosUltimaHora(idSensor) {
+
+}
+
+function mostrarSenha(campo) {
+
+    if (campo == 1) {
+
+        if (senhaUsuario.type == "password") {
+
+            senhaUsuario.type = "text";
+            o1.src = "../imagens/olho_a.png";
+        } else {
+
+            senhaUsuario.type = "password";
+            o1.src = "../imagens/olho_d.png";
+        }
+    } else if (campo == 2) {
+
+        if (confirmar_senha.type == "password") {
+
+            confirmar_senha.type = "text";
+            o2.src = "../imagens/olho_a.png";
+        } else {
+
+            confirmar_senha.type = "password";
+            o2.src = "../imagens/olho_d.png";
+        }
+    } else if (campo == 3) {
+
+        if (ipt_senha.type == "password") {
+
+            ipt_senha.type = "text";
+            o3.src = "../imagens/olho_a.png";
+        } else {
+
+            ipt_senha.type = "password";
+            o3.src = "../imagens/olho_d.png";
+        }
+    } else if (campo == 4) {
+
+        if (ipt_con_senha.type == "password") {
+
+            ipt_con_senha.type = "text";
+            o4.src = "../imagens/olho_a.png";
+        } else {
+
+            ipt_con_senha.type = "password";
+            o4.src = "../imagens/olho_d.png";
+        }
+    } else if (campo == 5) {
+
+        if (atual_senha.type == "password") {
+
+            atual_senha.type = "text";
+            o5.src = "../imagens/olho_a.png";
+        } else {
+
+            atual_senha.type = "password";
+            o5.src = "../imagens/olho_d.png";
+        }
+    } else if (campo == 6) {
+
+        if (nova_senha.type == "password") {
+
+            nova_senha.type = "text";
+            o6.src = "../imagens/olho_a.png";
+        } else {
+
+            nova_senha.type = "password";
+            o6.src = "../imagens/olho_d.png";
+        }
+    } else if (campo == 7) {
+
+        if (confirmar_senha.type == "password") {
+
+            confirmar_senha.type = "text";
+            o7.src = "../imagens/olho_a.png";
+        } else {
+
+            confirmar_senha.type = "password";
+            o7.src = "../imagens/olho_d.png";
+        }
+    }
 }
 
 function chamar() {
@@ -182,23 +264,27 @@ function chamar() {
     btnAnual.style.fontWeight = "normal";
     btnAnual.style.backgroundColor = "transparent";
 
+    btnMapa.style.borderBottom = "none";
+    btnMapa.style.fontWeight = "normal";
+    btnMapa.style.backgroundColor = "transparent";
+
     btnChamado.style.borderBottom = "solid 3px #D71B22";
     btnChamado.style.fontWeight = "bold";
     btnChamado.style.backgroundColor = "#023f79";
 }
 
-function chamar_config(){
+function chamar_config() {
     corpo_config.style.display = "none";
     chamado.style.display = "flex";
 
     botaoselecionado.style.borderBottom = "none";
     botaoselecionado.style.fontWeight = "normal";
     botaoselecionado.style.backgroundColor = "transparent";
-    
+
     empresaA.style.borderBottom = "none";
     empresaA.style.fontWeight = "normal";
     empresaA.style.backgroundColor = "transparent";
-    
+
     funcionarioA.style.borderBottom = "none";
     funcionarioA.style.fontWeight = "normal";
     funcionarioA.style.backgroundColor = "transparent";
@@ -208,7 +294,7 @@ function chamar_config(){
     btnChamado.style.backgroundColor = "#023f79";
 }
 
-function chamar_func(){
+function chamar_func() {
     corpo_config.style.display = "none";
     chamado.style.display = "flex";
     func.style.display = "flex";
@@ -216,11 +302,11 @@ function chamar_func(){
     botaoselecionado.style.borderBottom = "none";
     botaoselecionado.style.fontWeight = "normal";
     botaoselecionado.style.backgroundColor = "transparent";
-    
+
     empresaA.style.borderBottom = "none";
     empresaA.style.fontWeight = "normal";
     empresaA.style.backgroundColor = "transparent";
-    
+
     funcionarioA.style.borderBottom = "none";
     funcionarioA.style.fontWeight = "normal";
     funcionarioA.style.backgroundColor = "transparent";
@@ -230,8 +316,8 @@ function chamar_func(){
     btnChamado.style.backgroundColor = "#023f79";
 }
 
-function puxarFuncionarios(){
-    
+function puxarFuncionarios() {
+
     var usuarioSession = sessionStorage.USUARIO;
     var dadosUsuario = JSON.parse(usuarioSession);
     var idEmpresa = dadosUsuario.fkEmpresa;
@@ -243,7 +329,7 @@ function puxarFuncionarios(){
         },
         body: JSON.stringify({
             fkEmpresa: idEmpresa,
-           
+
         })
     }).then(function (resposta) {
         console.log("ESTOU NO THEN DO que pega dados do endereco da empresa!")
@@ -252,11 +338,11 @@ function puxarFuncionarios(){
             console.log(resposta);
 
             resposta.json().then(json => {
-                
+
                 console.log(JSON.stringify(json));
-                json.forEach( funcionario => {
+                json.forEach(funcionario => {
                     console.log(funcionario.nome)
-                    lista.innerHTML +=`
+                    lista.innerHTML += `
                     <div class="lista_item">
                     <ul>
                         <div class="lista_campo">
@@ -279,7 +365,7 @@ function puxarFuncionarios(){
                     `
                     acabarCarregar();
                 });
-            });    
+            });
         } else {
 
             console.log("Houve um erro ao tentar pegar os dados da empresa!");
@@ -288,7 +374,7 @@ function puxarFuncionarios(){
                 console.log(texto);
                 finalizarAguardar(texto);
             });
-        }         
+        }
     }).catch(function (erro) {
         console.log(erro);
         alert(erro)
@@ -296,24 +382,24 @@ function puxarFuncionarios(){
 
 }
 
-function auxiliar(funcionario){
+function auxiliar(funcionario) {
     console.log(funcionario)
-    location.href = "funcionario_editar.html?id="+funcionario;
+    location.href = "funcionario_editar.html?id=" + funcionario;
 }
 
-function deletar(funcionario, nome){
+function deletar(funcionario, nome) {
 
     Swal.fire({
         title: "Espera aí!",
         background: "#212121",
         color: "#fff",
-        text: "Deseja realmente apagar o(a) usuário(a) "+nome+"?",
+        text: "Deseja realmente apagar o(a) usuário(a) " + nome + "?",
         icon: "warning",
         showDenyButton: true,
         confirmButtonText: "Sim",
         denyButtonText: "Não",
-        
-      }).then((result) => {
+
+    }).then((result) => {
         /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
             cont.style.display = 'none';
@@ -328,7 +414,7 @@ function deletar(funcionario, nome){
                 }),
             }).then(function (resposta) {
                 console.log("ESTOU NO THEN DO que apaga o usuario!")
-        
+
                 if (resposta.ok) {
                     console.log(resposta);
                     cont.style.display = 'block';
@@ -342,11 +428,11 @@ function deletar(funcionario, nome){
                             text: "Usuário alterado com sucesso!",
                             icon: "success",
                             willClose: () => {
-                              location.href = "./funcionario.html";
+                                location.href = "./funcionario.html";
                             },
                             confirmButtonText: "Ok",
-                          });
-                    });    
+                        });
+                    });
                 } else {
                     Swal.fire({
                         title: "Erro!",
@@ -355,20 +441,20 @@ function deletar(funcionario, nome){
                         text: "Houve um erro ao tentar realizar a alteração!",
                         icon: "error",
                         confirmButtonText: "Ok",
-                      });
+                    });
                     console.log("Houve um erro ao tentar pegar os dados da empresa!");
-        
+
                     resposta.text().then(texto => {
                         console.log(texto);
                         finalizarAguardar(texto);
                     });
-                }         
+                }
             });
         } else if (result.isDenied) {
-          Swal.fire('Changes are not saved', '', 'info')
+            Swal.fire('Changes are not saved', '', 'info')
         }
-      })
+    })
 
-    
+
 }
 
